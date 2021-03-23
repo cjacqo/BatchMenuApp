0c955297729a33b12f77525fdcdc61d166b4bca3
cjacqo <contact.jayden.cipriano@gmail.com>
2021-03-22 22:00:10 -0700

    ProductRepository Object

---
+++ batchmenuapp/src/main/java/com/cjaqo/batchmenuapp/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.cjaqo.batchmenuapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cjaqo.batchmenuapp.domain.Product;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+	@Override
+	Iterable<Product> findAllById(Iterable<Long> iterable);
+	
+	Product findByProductBatchNumber(String product);
+	
+	@Override
+	Iterable<Product> findAll();
+	
+}
