1bf310c19874068816fa8adc7cb2cbd602a2a2ec
cjacqo <contact.jayden.cipriano@gmail.com>
2021-03-22 21:29:17 -0700

    first commit

---
+++ batchmenuapp/src/main/java/com/cjaqo/batchmenuapp/BatchmenuappApplication.java
@@ -0,0 +1,13 @@
+package com.cjaqo.batchmenuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchmenuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchmenuappApplication.class, args);
+	}
+
+}
---
+++ batchmenuapp/src/test/java/com/cjaqo/batchmenuapp/BatchmenuappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjaqo.batchmenuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchmenuappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
