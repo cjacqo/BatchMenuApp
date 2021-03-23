bdca36bd3913ff8438037d78a0db4d8cd59c2196
cjacqo <76092910+cjacqo@users.noreply.github.com>
2021-03-23 00:28:50 -0700

    Update BatchmenuappApplication.java

--- batchmenuapp/src/main/java/com/cjaqo/batchmenuapp/BatchmenuappApplication.java
+++ batchmenuapp/src/main/java/com/cjaqo/batchmenuapp/BatchmenuappApplication.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.cjaqo.batchmenuapp.components"})
-@EntityScan("com.cjaqo.batchmenuapp.domain")
-@EnableJpaRepositories("com.cjaqo.batchmenuapp.repository")
 public class BatchmenuappApplication {
 
 	public static void main(String[] args) {
